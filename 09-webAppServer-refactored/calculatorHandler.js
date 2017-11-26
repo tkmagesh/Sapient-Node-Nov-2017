4b8cc66cc6f17f237205e3cc02a02097d007cf37
Magesh <tkmagesh77@gmail.com>
2017-11-26 11:33:49 +0530

    refactoring - complete

--- 09-webAppServer-refactored/calculatorHandler.js
+++ 09-webAppServer-refactored/calculatorHandler.js
@@ -1,32 +1,17 @@
 var querystring = require('querystring'),
 	calculator = require('./calculator');
 
-module.exports = function(req, res){
-	var resourceName = req.urlObj.pathname,
-		urlObj = req.urlObj;
-	if (resourceName === '/calculator' && req.method === 'GET'){
-		var reqData = querystring.parse(urlObj.query),
-			op = reqData.op,
-			n1 = parseInt(reqData.n1),
-			n2 = parseInt(reqData.n2),
+module.exports = function(req, res, next){
+	var resourceName = req.urlObj.pathname;
+	if (resourceName === '/calculator'){
+		var data = req.method === 'GET' ?  req.query : req.body,
+			op = data.op,
+			n1 = parseInt(data.n1),
+			n2 = parseInt(data.n2),
 			result = calculator[op](n1, n2);
-
 		res.write(result.toString());
 		res.end();
-	} else if (resourceName === '/calculator' && req.method === 'POST'){
-		var rawData = '';
-		req.on('data', function(contents){
-			rawData += contents;
-		});
-		req.on('end', function(){
-			var reqData = querystring.parse(rawData),
-				op = reqData.op,
-				n1 = parseInt(reqData.n1),
-				n2 = parseInt(reqData.n2),
-				result = calculator[op](n1, n2);
-
-			res.write(result.toString());
-			res.end();	
-		});
+	} else {
+		next();
 	}
 }
--- 09-webAppServer-refactored/server.js
+++ 09-webAppServer-refactored/server.js
@@ -1,17 +1,15 @@
 var http = require('http'),
+	path = require('path'),
+	app = require('./app'),
 	dataParser = require('./dataParser'),
 	serveStatic = require('./serveStatic'),
 	calculatorHandler = require('./calculatorHandler'),
 	notFoundHandler = require('./notFoundHandler');
 
-
-
-var server = http.createServer(function(req, res){
-	dataParser(req);
-	serveStatic(req, res);
-	calculatorHandler(req, res);
-	notFoundHandler(res);
-});
-
-server.listen(8080);
+app.use(dataParser);
+app.use(serveStatic(path.join(__dirname, 'public')));
+app.use(calculatorHandler);
+app.use(notFoundHandler);
+	
+http.createServer(app).listen(8080);
 console.log('server listeninig on port 8080!');
